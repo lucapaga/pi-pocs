86dc021bd8781602861a0f27260bd4c273fcdf73
Luca Paganelli <luca.paga@gmail.com>
2017-12-09 15:50:20 +0100

    getting great

--- 03_rest_service_w_music.py
+++ 03_rest_service_w_music.py
@@ -1,42 +1,55 @@
 #!flask/bin/python
 from flask import Flask, jsonify
-#from flask_cors import CORS
 import RPi.GPIO as GPIO
 import time
 import sys
+
+
+BuzzerPin = 3
+
+CL = [0, 131, 147, 165, 175, 196, 211, 248] # Low C Note Frequency
+CM = [0, 262, 294, 330, 350, 393, 441, 495] # Middle C Note Frequency
+CH = [0, 525, 589, 661, 700, 786, 882, 990] # High C Note Frequency
+
+song_1 = [ CM[3], CM[5], CM[6], CM[3], CM[2], CM[3], CM[5], CM[6], # Sound Notes 1
+          CH[1], CM[6], CM[5], CM[1], CM[3], CM[2], CM[2], CM[3],
+          CM[5], CM[2], CM[3], CM[3], CL[6], CL[6], CL[6], CM[1],
+          CM[2], CM[3], CM[2], CL[7], CL[6], CM[1], CL[5] ]
+
+beat_1 = [ 1, 1, 3, 1, 1, 3, 1, 1, # Beats of song 1, 1 means 1/8 beats
+          1, 1, 1, 1, 1, 1, 3, 1,
+          1, 3, 1, 1, 1, 1, 1, 1,
+          1, 2, 1, 1, 1, 1, 1, 1,
+          1, 1, 3 ]
+
+song_2 = [ CM[1], CM[1], CM[1], CL[5], CM[3], CM[3], CM[3], CM[1], # Sound Notes 2
+          CM[1], CM[3], CM[5], CM[5], CM[4], CM[3], CM[2], CM[2],
+          CM[3], CM[4], CM[4], CM[3], CM[2], CM[3], CM[1], CM[1],
+          CM[3], CM[2], CL[5], CL[7], CM[2], CM[1] ]
+
+beat_2 = [ 1, 1, 2, 2, 1, 1, 2, 2, # Beats of song 2, 1 means 1/8 beats
+          1, 1, 2, 2, 1, 1, 3, 1,
+          1, 2, 2, 1, 1, 2, 2, 1,
+          1, 2, 2, 1, 1, 3 ]
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(BuzzerPin, GPIO.OUT) # Set pins' mode is output
+
+
+
 
 app = Flask(__name__)
-#CORS(app)
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
-
-@app.route('/piraw/<int:io_slot>/<led_state>')
-def piraw(io_slot, led_state):
- gpio_channel=io_slot
- gpio_target_state=GPIO.LOW
- target_state='off'
- default_state=False
-
- if led_state.lower() == "on":
-  gpio_target_state=GPIO.HIGH
-  target_state='on'
- elif led_state.lower() == "off":
-  gpio_target_state=GPIO.LOW
- else:
-  print "Unknown LED STATE (" + led_state + "), defaulting to OFF"
-  default_state=True
-
- operate_gpio(gpio_channel, gpio_target_state)
- return jsonify({'result': 'OK', 'io_slot': { 'position': gpio_channel, 'state': { 'value': target_state, 'default': default_state}}})
-
+@app.route('/pi/play-music')
+def pi_sound():
 
 @app.route('/pi/<led_color>/<led_state>')
 def pi(led_color, led_state):
---
+++ 05_gpiozero_m_button.py
@@ -0,0 +1,20 @@
+from gpiozero import LED, Button
+from time import sleep
+
+def on_button_pressed(button):
+ print("Pulsante premuto!")
+
+green_led = LED(18)
+red_led = LED(17)
+
+button = Button(23)
+button.when_pressed = on_button_pressed
+
+green_led.on()
+sleep(1)
+red_led.on()
+
+sleep(5)
+
+red_led.off()
+green_led.off()
