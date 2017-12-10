126a9ec1652a36565251c8c53b243291f357cffd
Luca Paganelli <luca.paga@gmail.com>
2017-12-10 12:04:59 +0100

    going buzzed

--- 07_btn_and_led.py
+++ 07_btn_and_led.py
@@ -1,4 +1,4 @@
-from gpiozero import LED, Button
+from gpiozero import LED, Button, Buzzer
 from time import sleep
 
 def on_button_pressed(button):
@@ -7,14 +7,16 @@ def on_button_pressed(button):
      print("LED is ON, switching OFF!")
      red_led.off()
      green_led.off()
+     bz.beep(on_time=.2, off_time=.1, n=3, background=False)
  else:
      print("LED is OFF, switching ON!")
      green_led.on()
      red_led.on()
+     bz.beep(on_time=.2, off_time=.1, n=2, background=False)
 
 green_led = LED(18)
 red_led = LED(17)
-
+bz = Buzzer(3)
 button = Button(23)
 button.when_pressed = on_button_pressed
 
