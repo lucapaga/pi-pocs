54d5eeb736944ea8bfb316ffcc665a4b764a0356
Luca Paganelli <luca.paga@gmail.com>
2017-12-10 21:29:49 +0100

    brand new REST services

--- 07_btn_and_led.py
+++ 07_btn_and_led.py
@@ -8,18 +8,18 @@ def on_button_pressed(button):
      red_led.off()
      green_led.off()
      #bz.beep(on_time=.5, off_time=.2, n=3, background=False)
-     bz.beep()
-     sleep(1)
-     bz.beep()
+     #bz.beep()
+     #sleep(1)
+     #bz.beep()
  else:
      print("LED is OFF, switching ON!")
      green_led.on()
      red_led.on()
-     bz.beep()
+     #bz.beep()
 
 green_led = LED(18)
 red_led = LED(17)
-bz = Buzzer(3)
+#bz = Buzzer(3)
 button = Button(23)
 button.when_pressed = on_button_pressed
 
