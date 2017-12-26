5c685e4d79fc8ec6bdbe8d41fa5fa76ed03a70f8
Luca Paganelli <luca.paga@gmail.com>
2017-12-26 23:56:20 +0100

    final

--- pi-pubsub/run-daemon.sh
+++ pi-pubsub/run-daemon.sh
@@ -6,4 +6,5 @@ export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/sa/pi-pocs-pubsub-sa01-00afc7e81369
 python 01_pubsub_gpiozero.py \
       --project luca-paganelli-formazione \
       --commands_topic_name gpio_commands_topic \
+      --commands_subscription_name gpio_commands_subscription \
       --status_topic_name gpio_status_topic
