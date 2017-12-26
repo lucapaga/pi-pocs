15766dc054667dba67449e92b9cb2cf3a976b7ee
Luca Paganelli <luca.paga@gmail.com>
2017-12-26 12:51:48 +0100

    creating the pubsub daemon

---
+++ pi-pubsub/01_pubsub_gpiozero.py
@@ -0,0 +1,159 @@
+#!flask/bin/python
+import sys
+import argparse
+import time
+
+from google.cloud import pubsub_v1
+from gpiozero import LED, Button
+
+
+def create_subscription(project, topic_name, subscription_name, client):
+    subscriber = client
+    topic_path = subscriber.topic_path(project, topic_name)
+    subscription_path = subscriber.subscription_path(
+        project, subscription_name)
+    subscription = subscriber.create_subscription(
+        subscription_path, topic_path)
+    print('Subscription created: {}'.format(subscription))
+
+
+def delete_subscription(project, subscription_name, client):
+    subscriber = client
+    subscription_path = subscriber.subscription_path(
+        project, subscription_name)
+    subscriber.delete_subscription(subscription_path)
+    print('Subscription deleted: {}'.format(subscription_path))
+
+
+def publish_message(project, topic_name, message, client):
+#    publisher = pubsub_v1.PublisherClient()
+    publisher = client
+    topic_path = publisher.topic_path(project, topic_name)
+    data = u'{}'.format(message)
+    data = data.encode('utf-8')
+    publisher.publish(topic_path, data=data)
+    print('Published messages.')
+
+
+def on_pubsub_message(message):
+    print('Received COMMAND: {}'.format(message))
+    message.ack()
+    aCommand = message.data
+
+    theLED = None
+    if aCommand.led_color.lower() == "green":
+        theLED = green_led
+        print("Working on GREEN led")
+    elif aCommand.led_color.lower() == "red":
+        theLED = red_led
+        print("Working on GREEN led")
+    else:
+        print("Unkown LED color: {}".format(aCommand.led_color))
+
+    if theLED != None:
+        if aCommand.action == "light-on":
+            print("Switching the LED on")
+            theLED.on()
+        elif aCommand.action == "light-off":
+            print("Switching the LED off")
+            theLED.off()
+        else:
+            print("Unkown ACTION: {}".format(aCommand.action))
+
+
+def run_logic(args):
+    green_led = LED(args.green_led_pin)
+    red_led = LED(args.red_led_pin)
+    button = Button(args.push_button_pin)
+
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = None
+    subscription_name = None
+
+    if args.commands_subscription_name == None or args.commands_subscription_name == "":
+        subscription_name="{}_subscr_01".format(args.commands_topic_name)
+        subscription_path = create_subscription(
+            args.project,
+            args.commands_topic_name,
+            subscription_name,
+            subscriber)
+    else:
+        subscription_name = args.commands_subscription_name
+        subscription_path = subscriber.subscription_path(
+            args.project,
+            subscription_name)
+
+    print("================================================")
+    print(" Creating PUB/SUB subsription for 'COMANDS' ...")
+    print("------------------------------------------------")
+    print("         PROJECT: {}".format(args.project))
+    print("           TOPIC: {}".format(args.commands_topic_name))
+    print("    SUBSCRIPTION: {}".format(subscription_name))
+    print("    FLOW CONTROL: {}".format(args.max_batch_size))
+    print("================================================")
+
+    flow_control = pubsub_v1.types.FlowControl(max_messages=args.max_batch_size)
+    subscriber.subscribe(
+        subscription_path,
+        callback=on_pubsub_message,
+        flow_control=flow_control)
+
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        print("================================================")
+        print(" Stopping deamon ...")
+        print("------------------------------------------------")
+        print("  REMOVING SUBSCRIPTION: '{}'...".format(subscription_name))
+        delete_subscription(args.project, subscription_name, subscriber)
+        print("================================================")
+
+
+green_led = None
+red_led = None
+button = None
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+            '--project',
+            default=os.environ.get('GOOGLE_CLOUD_PROJECT'),
+            help='GCP cloud project name')
+    parser.add_argument(
+            '--commands_topic_name',
+            required=True,
+            help='PUB/SUB TOPIC for COMMANDS')
+    parser.add_argument(
+            '--commands_subscription_name',
+            help='PUB/SUB SUBCRIPTION for COMMANDS')
+    parser.add_argument(
+            '--status_topic_name',
+            required=True,
+            help='PUB/SUB TOPIC for STATUS')
+    parser.add_argument(
+            '--green_led_pin',
+            type=int,
+            default=18,
+            help='GPIO PIN for GREEN LED')
+    parser.add_argument(
+            '--red_led_pin',
+            type=int,
+            default=17,
+            help='GPIO PIN for RED LED')
+    parser.add_argument(
+            '--push_button_pin',
+            type=int,
+            default=23,
+            help='GPIO PIN for PUSH BUTTON')
+    parser.add_argument(
+            '--max_batch_size',
+            type=int,
+            default=10,
+            help='Number of messagges pulled from PUB/SUB (max)')
+    args = parser.parse_args()
+    run_logic(args)
---
+++ pi-pubsub/08_rest_gpiozero.py
@@ -0,0 +1,123 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+import sys
+
+from gpiozero import LED, Button
+#from time import sleep
+
+GREEN_LED_NR=18
+RED_LED_NR=17
+BUTTON_NR=23
+
+green_led = LED(GREEN_LED_NR)
+red_led = LED(RED_LED_NR)
+button = Button(BUTTON_NR)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Up'n'Running!"
+
+
+@app.route('/piall/<led_state>')
+def piall(led_state):
+ if led_state.lower() == "on":
+  red_led.on()
+  green_led.on()
+ elif led_state.lower() == "off":
+  red_led.off()
+  green_led.off()
+ else:
+  print "Unknown LED STATE (" + led_state + "), defaulting to OFF"
+  red_led.off()
+  green_led.off()
+ return pistate()
+
+
+@app.route('/pitoggle/all')
+def pitoggle():
+ red_led.toggle()
+ green_led.toggle()
+ return pistate()
+
+
+@app.route('/pistate')
+def pistate():
+ return jsonify({'result': 'OK', 'io_slots': [
+                    { 'position': GREEN_LED_NR, 'state': { 'value': green_led.is_lit}}
+                    , { 'position': RED_LED_NR, 'state': { 'value': red_led.is_lit}}]})
+
+
+@app.route('/piraw/<int:io_slot>/<led_state>')
+def piraw(io_slot, led_state):
+ gpio_channel=io_slot
+ default_state=False
+
+ theLED = None
+ if(io_slot == GREEN_LED_NR):
+     theLED = green_led
+ else:
+     theLED = red_led
+
+ if led_state.lower() == "on":
+  theLED.on()
+ elif led_state.lower() == "off":
+  theLED.off()
+ else:
+  print "Unknown LED STATE (" + led_state + "), defaulting to OFF"
+  theLED.off()
+  default_state=True
+
+ return jsonify({'result': 'OK', 'io_slot': { 'position': gpio_channel, 'state': { 'value': theLED.is_lit, 'default': default_state}}})
+
+
+@app.route('/pi/<led_color>/<led_state>')
+def pi(led_color, led_state):
+ print "Processing LED '" + led_color + "' to STATE '" + led_state + "'"
+ default_state=False
+
+ theLED = None
+ if led_color.lower() == "green":
+  print "Operating GREEN LED ..."
+  theLED = green_led
+ elif led_color.lower() == "red":
+  print "Operating RED LED ..."
+  theLED = red_led
+ else:
+  print "Unknown color: " + led_color
+  return jsonify({'result': 'KO', 'error':'Unknown LED color'})
+
+ if led_state.lower() == "on":
+  theLED.on()
+ elif led_state.lower() == "off":
+  theLED.off()
+ else:
+  print "Unknown LED STATE (" + led_state + "), defaulting to OFF"
+  theLED.off()
+  default_state=True
+
+ return jsonify({'result': 'OK', 'led': { 'color': led_color.lower(), 'state': { 'value': theLED.is_lit, 'default': default_state}}})
+
+
+
+def on_button_pressed(button):
+ print("Pulsante premuto!")
+ if green_led.is_lit:
+     print("LED is ON, switching OFF!")
+     red_led.off()
+     green_led.off()
+     #bz.beep(on_time=.5, off_time=.2, n=3, background=False)
+     #bz.beep()
+     #sleep(1)
+     #bz.beep()
+ else:
+     print("LED is OFF, switching ON!")
+     green_led.on()
+     red_led.on()
+     #bz.beep()
+
+button.when_pressed = on_button_pressed
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
