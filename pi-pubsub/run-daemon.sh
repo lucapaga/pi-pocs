9d2984640df3fd8022ac80c7cc67369058251637
Luca Paganelli <luca.paga@gmail.com>
2017-12-27 14:05:45 +0100

    sh

--- pi-pubsub/run-daemon.sh
+++ pi-pubsub/run-daemon.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 #source ${HOME}/env/bin/activate
+echo "Setting up security using '${GCP_APP_CRED_JSON_PATH}' ..."
 export GOOGLE_APPLICATION_CREDENTIALS=${GCP_APP_CRED_JSON_PATH}
+
+
+echo "Running daemon ... "
+echo " - EMULATION MODE:  ${PI_EMULATE_GPIO}"
+echo " - GCP Project:     ${GCP_PROJECT_NAME}"
+echo " - COMMANDS:"
+echo "     - TOPIC:       ${GCP_PUBSUB_TOPIC_COMMANDS}"
+echo "     - SUBCRIPTION: ${GCP_PUBSUB_SUBSCRIPTION_COMMANDS}"
+echo " - STAUS:"
+echo "     - TOPIC:       ${GCP_PUBSUB_TOPIC_STATUS}"
+echo ""
+echo ""
 
 python 01_pubsub_gpiozero.py \
       --project ${GCP_PROJECT_NAME} \
@@ -9,3 +22,6 @@ python 01_pubsub_gpiozero.py \
       --commands_subscription_name ${GCP_PUBSUB_SUBSCRIPTION_COMMANDS} \
       --status_topic_name ${GCP_PUBSUB_TOPIC_STATUS} \
       --emulate_gpio ${PI_EMULATE_GPIO}
+
+echo "DONE!"
+echo "Exiting ..."
