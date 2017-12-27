45b194d282f5dae70f5b3d0d00345b16ce1130c5
Luca Paganelli <luca.paga@gmail.com>
2017-12-27 13:56:32 +0100

    testing on PI

---
+++ pi-pubsub/run-daemon-off-docker-on-pi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export GCP_APP_CRED_JSON_PATH=$(pwd)/sa/pi-pocs-pubsub-sa01-00afc7e81369.json
+export GCP_PROJECT_NAME=luca-paganelli-formazione
+export GCP_PUBSUB_TOPIC_COMMANDS=gpio_commands_topic
+export GCP_PUBSUB_SUBSCRIPTION_COMMANDS=gpio_commands_subscription
+export GCP_PUBSUB_TOPIC_STATUS=gpio_status_topic
+export PI_EMULATE_GPIO=False
+
+./run-daemon.sh
